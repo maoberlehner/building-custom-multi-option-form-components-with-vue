df4e0ef8398643cb0acb9f64ad1bc5bedc8ee6d9
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-05-05 14:28:57 +0200

    Add new tags

--- src/components/FormTagsInput.vue
+++ src/components/FormTagsInput.vue
@@ -16,6 +16,10 @@
         </button>
       </li>
     </ul>
+    <input
+      v-model="newTag"
+      @keydown.enter="addTag"
+    >
   </div>
 </template>
 
@@ -25,9 +29,40 @@ import formOptionSelectMixin from './mixins/form-option-select-mixin';
 export default {
   name: `FormTagsInput`,
   mixins: [formOptionSelectMixin],
+  props: {
+    newTagAdapter: {
+      type: Function,
+      default: value => value,
+    },
+  },
+  data() {
+    return {
+      newTag: ``,
+    };
+  },
   computed: {
+    newTagOption() {
+      return this.optionAdapter(this.newTagValue);
+    },
+    newTagValue() {
+      const trimmedNewTag = this.newTag.trim();
+
+      return trimmedNewTag.length > 0
+        ? this.newTagAdapter(trimmedNewTag)
+        : null;
+    },
     tagOptions() {
       return this.value.map(x => this.optionAdapter(x));
+    },
+  },
+  methods: {
+    addTag() {
+      if (!this.newTagValue || this.tagOptions.find(({ id }) => id === this.newTagOption.id)) {
+        return;
+      }
+
+      this.$emit(`change`, [...this.value, this.newTagValue]);
+      this.newTag = ``;
     },
   },
 };
